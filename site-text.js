f4151a90b9a5f7d53903082edb4b84dea5ec03b1
takaoYC <minikid.kyoshishikaku@gmail.com>
2026-08-30 23:13:42 +0800

    Add files via upload

--- site-text.js
+++ site-text.js
@@ -25,5 +25,5 @@ const SITE = {
   "footer.fin": "PP YAO · 12TH ANNIVERSARY",
   "footer.sub": "FROM THE FIRST STAR, AND ONWARD",
   "footer.orn": "✦ &nbsp; ✧ &nbsp; ✦",
-  "footer.ver": "ver 2026.09.01-2"
+  "footer.ver": "ver 2026.09.01-3"
 };
